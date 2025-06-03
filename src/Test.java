2cd5578547a7c201351bc817ed13555aec490c78
drmathwin <tlfielder@gmail.com>
2025-06-03 15:50:10 -0500

    Initial code upload.

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Test {
+	public Test() {
+		javafx.application.Application.launch(TestApp.class);
+	}
+
+	public static void main(String[] args) {
+		new Test();
+	}
+}
